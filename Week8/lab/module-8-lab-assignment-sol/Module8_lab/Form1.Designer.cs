2e1dab2419c668e12d7a168dac093ff1bbcb424c
Ali Valiyev <37627092+AliValiyev@users.noreply.github.com>
2022-11-24 14:45:09 +0300

    Add files via upload

---
+++ Week8/lab/module-8-lab-assignment-sol/Module8_lab/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Module8_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.lbl_message = new System.Windows.Forms.Label();
+            this.lbl_length = new System.Windows.Forms.Label();
+            this.lbl_upper = new System.Windows.Forms.Label();
+            this.lbl_lower = new System.Windows.Forms.Label();
+            this.lbl_digits = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(43, 38);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(136, 23);
+            this.txt_password.TabIndex = 5;
+            this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.AutoSize = true;
+            this.lbl_message.Location = new System.Drawing.Point(46, 90);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(133, 15);
+            this.lbl_message.TabIndex = 3;
+            this.lbl_message.Text = "Password Requirements";
+            // 
+            // lbl_length
+            // 
+            this.lbl_length.AutoSize = true;
+            this.lbl_length.Location = new System.Drawing.Point(64, 113);
+            this.lbl_length.Name = "lbl_length";
+            this.lbl_length.Size = new System.Drawing.Size(166, 15);
+            this.lbl_length.TabIndex = 6;
+            this.lbl_length.Text = "Password length is at least 10. ";
+            // 
+            // lbl_upper
+            // 
+            this.lbl_upper.AutoSize = true;
+            this.lbl_upper.Location = new System.Drawing.Point(64, 132);
+            this.lbl_upper.Name = "lbl_upper";
+            this.lbl_upper.Size = new System.Drawing.Size(240, 15);
+            this.lbl_upper.TabIndex = 7;
+            this.lbl_upper.Text = "The number of uppercase letters is at least 3.";
+            // 
+            // lbl_lower
+            // 
+            this.lbl_lower.AutoSize = true;
+            this.lbl_lower.Location = new System.Drawing.Point(64, 151);
+            this.lbl_lower.Name = "lbl_lower";
+            this.lbl_lower.Size = new System.Drawing.Size(232, 15);
+            this.lbl_lower.TabIndex = 8;
+            this.lbl_lower.Text = "The number of lowercase letters is at lest 2.";
+            // 
+            // lbl_digits
+            // 
+            this.lbl_digits.AutoSize = true;
+            this.lbl_digits.Location = new System.Drawing.Point(64, 170);
+            this.lbl_digits.Name = "lbl_digits";
+            this.lbl_digits.Size = new System.Drawing.Size(227, 15);
+            this.lbl_digits.TabIndex = 9;
+            this.lbl_digits.Text = "The number of numeric digits is at least 2.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 221);
+            this.Controls.Add(this.lbl_digits);
+            this.Controls.Add(this.lbl_lower);
+            this.Controls.Add(this.lbl_upper);
+            this.Controls.Add(this.lbl_length);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.lbl_message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label lbl_message;
+        private System.Windows.Forms.Label lbl_length;
+        private System.Windows.Forms.Label lbl_upper;
+        private System.Windows.Forms.Label lbl_lower;
+        private System.Windows.Forms.Label lbl_digits;
+    }
+}
+
---
+++ Week8/lab/module-8-lab-assignment-sol/Module8_lab/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module8_lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This  method should add ☑ to the end of labelText and return it
+        private string AddCheckBoxToMessage(string labelText)
+        {
+            //Check if labelText contains ❌, if so delete ❌
+            //YOUR CODE GOES HERE
+            int index = labelText.IndexOf("❌");
+            if (index != -1)
+            {
+                labelText = labelText.Remove(index);
+            }
+
+            //Check if labelText contains ☑, if not add ☑ to the end of labelText
+            //YOUR CODE GOES HERE
+            if (!labelText.Contains("☑"))
+            {
+                labelText = labelText.Insert(labelText.Length - 1, "☑");
+            }
+
+
+            return labelText;
+        }
+
+        //This  method should add ❌ to the end of labelText and return it
+        private string AddCrossToMessage(string labelText)
+        {
+            //Check if labelText contains ☑, if so delete ☑
+            //YOUR CODE GOES HERE
+            int index = labelText.IndexOf("☑");
+            if (index != -1)
+            {
+                labelText = labelText.Remove(index);
+            }
+
+            //Check if labelText contains ❌, if not add ❌ to the end of labelText
+            //YOUR CODE GOES HERE
+            if (!labelText.Contains("❌"))
+            {
+                labelText = labelText.Insert(labelText.Length - 1, "❌");
+            }
+            return labelText;
+        }
+
+        private void txt_password_TextChanged(object sender, EventArgs e)
+        {
+            //CHECK THE LENGHT REQUIREMENT
+            //Write the if-else condition in which you need to call CheckLength.
+            ////(1)If CheckLength returns TRUE,
+            ////////(1.1)make a proper call to AddCheckBoxToMessage
+            ////////(1.2)update the lbl_length text
+            ////////(1.3)set the font color of lbl_length text to Green.
+            ////(2)If CheckLength returns FALSE
+            ////////(2.1)make a proper call to AddCrossToMessage
+            ////////(2.2)update the lbl_length text
+            ////////(2.3)set the font color of lbl_length text to Red.
+            //YOUR CODE GOES HERE
+            if (CheckLength(txt_password.Text))
+            {
+                AddCheckBoxToMessage(txt_password.Text);
+                lbl_length.Text = "Password length is valid!";
+                lbl_length.ForeColor = Color.Green;
+            }
+            else
+            {
+                AddCrossToMessage(txt_password.Text);
+                lbl_length.Text = "Password length should be at least 10!";
+                lbl_length.ForeColor = Color.Red;
+            }
+            //CHECK THE UPPERCASE REQUIREMENT
+            //YOUR CODE GOES HERE
+            if (CheckUpperCase(txt_password.Text))
+            {
+                AddCheckBoxToMessage(txt_password.Text);
+                lbl_upper.Text = "Password have valid number of uppercase letters!";
+                lbl_upper.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                AddCrossToMessage(txt_password.Text);
+                lbl_upper.Text = "Password should contain at least 3 uppercase letters!";
+                lbl_upper.ForeColor = Color.Red;
+            }
+            //CHECK THE LOWERCASE REQUIREMENT
+            //YOUR CODE GOES HERE
+            if (CheckLowerCase(txt_password.Text))
+            {
+                AddCheckBoxToMessage(txt_password.Text);
+                lbl_lower.Text = "Password have valid number of lowercase letters!";
+                lbl_lower.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                AddCrossToMessage(txt_password.Text);
+                lbl_lower.Text = "Password should contain at least 2 lowercase letters!";
+                lbl_lower.ForeColor = Color.Red;
+            }
+            //CHECK THE NUMERIC REQUIREMENT
+            //YOUR CODE GOES HERE
+            if (CheckDigit(txt_password.Text))
+            {
+                AddCheckBoxToMessage(txt_password.Text);
+                lbl_digits.Text = "Password have valid number of numeric digits!";
+                lbl_digits.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                AddCrossToMessage(txt_password.Text);
+                lbl_digits.Text = "Password should contain at least 2 numbers!";
+                lbl_digits.ForeColor = Color.Red;
+            }
+
+
+            //OPTIONAL FUNCTIONALITY//
+            //This part will not be graded!//
+            //Once all requirements are met, do not let users enter new text.
+            if (CheckLength(txt_password.Text) && CheckUpperCase(txt_password.Text) && CheckLowerCase(txt_password.Text) && CheckDigit(txt_password.Text))
+            {
+                txt_password.ReadOnly = true;
+            }
+        }
+
+        #region Method for Checking Requirements
+        //All methods for checking various requirements are already defined below.
+        //All these methods return true if the requirement is satisfied.
+
+        private bool CheckLength(string password)
+        {
+            return (password.Length >= 10);
+        }
+
+        private bool CheckUpperCase(string password)
+        {
+            int upperCaseCount = 0;
+
+            foreach (char ch in password)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCaseCount++;
+                }
+            }
+
+            return (upperCaseCount >= 3);
+        }
+
+        private bool CheckLowerCase(string password)
+        {
+            int lowerCaseCount = 0;
+
+            for (int x = 0; x < password.Length; x++)
+            {
+                if (char.IsLower(password, x))
+                {
+                    lowerCaseCount++;
+                }
+            }
+
+            return (lowerCaseCount >= 2);
+        }
+
+        private bool CheckDigit(string password)
+        {
+            int digitCount = 0;
+
+            foreach (char ch in password)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digitCount++;
+                }
+            }
+
+            return (digitCount >= 2);
+        }
+        #endregion
+    }
+}
