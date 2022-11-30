e69faa0da4774d32e0bc906211a9ac8a33bcce4c
Ali Valiyev <37627092+AliValiyev@users.noreply.github.com>
2022-11-30 20:52:38 +0300

    Add files via upload

---
+++ Week9/lab/module-9-lab-assignment-sol/Module9_Lab/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module9_Lab.Model
+{
+    internal class Person
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birthyear { get; set; }
+        public string Birthplace { get; set; }
+    }
+}
